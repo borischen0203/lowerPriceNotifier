1f77eb40be46f2101da2818fdf7b22c1a779919d
Boris Chen <borischen0203@gmail.com>
2023-05-12 13:40:02 +0100

    Add home api and requirement.txt

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return "hello world"
+
 @app.route('/health', methods=['GET'])
 def health_check():
     return 'OK', 200

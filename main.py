b90b797948e2d5bcd08905d52ec1d842590fd8e3
Boris Chen <borischen0203@gmail.com>
2023-05-12 13:27:50 +0100

    Add health check api

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return 'OK', 200
+
+if __name__ == '__main__':
+    host = os.environ.get('HOST', 'localhost')
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host=host, port=port)

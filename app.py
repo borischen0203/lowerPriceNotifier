109c05402d9a63e5deb670e9a8823cce6074b665
Boris Chen <borischen0203@gmail.com>
2023-05-13 00:05:01 +0100

    Remove vercel.json

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return "Home page"
 
 @app.route('/health', methods=['GET'])
 def health_check():
     return 'OK', 200
+
+if __name__ == '__main__':
+    app.run()
 
 # if __name__ == '__main__':
 #     host = os.environ.get('HOST', 'localhost')
